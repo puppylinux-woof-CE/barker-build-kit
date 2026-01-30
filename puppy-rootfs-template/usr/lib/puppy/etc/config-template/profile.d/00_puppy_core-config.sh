e14ebe9a8e75dea8604249125f246cafe9186f70
Rizal Martin <49640119+rizalmart@users.noreply.github.com>
2026-01-30 08:39:53 +0000

    Add files via upload

--- puppy-rootfs-template/usr/lib/puppy/etc/config-template/profile.d/00_puppy_core-config.sh
+++ puppy-rootfs-template/usr/lib/puppy/etc/config-template/profile.d/00_puppy_core-config.sh
@@ -1,7 +1,32 @@
 [ "$LC_ALL" == "" ] && export LC_ALL=C
 [ "$LC_CTYPE" == "" ] && export LC_CTYPE=C
 
-export PATH="/usr/lib/puppy/sbin:/usr/lib/puppy/bin:/usr/sbin/overrides:/usr/bin/overrides:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/sbin/32:/usr/bin/32:/usr/games:/root/my-applications/bin"
+export PATH="/usr/lib/puppy/bin:/usr/lib/puppy/sbin:/usr/bin/overrides:/usr/sbin/overrides:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/bin/32:/usr/sbin/32:/usr/games:/root/my-applications/bin"
+
+
+if [ "$LD_LIBRARY_PATH" == "" ]; then
+
+	LD_LIBRARY_PATH32="/usr/local/lib32:/usr/local/libx32:/usr/local/lib/i386-linux-gnu:/usr/local/lib:/usr/lib32:/usr/libx32:/usr/lib/i386-linux-gnu:/usr/lib:/lib32:/libx32:/lib/i386-linux-gnu:/lib:/root/my-applications/lib"
+	LD_LIBRARY_PATH64="/usr/local/lib64:/usr/local/lib/x86_64-linux-gnu:/usr/local/lib:/usr/lib64:/usr/lib/x86_64-linux-gnu:/usr/lib:/lib64:/lib/x86_64-linux-gnu:/lib:/root/my-applications/lib"
+	LD_LIBRARY_PATH64_COMPAT="/usr/local/lib32:/usr/local/libx32:/usr/local/lib/i386-linux-gnu:/usr/lib32:/usr/libx32:/usr/lib/i386-linux-gnu:/lib32:/libx32:/lib/i386-linux-gnu"
+		
+	if [ -e /lib64/libc.so.6 ] || [ -e /lib/x86_64-linux-gnu/libc.so.6 ]; then #slackware64
+		LD_LIBRARY_PATH="${LD_LIBRARY_PATH64}:${LD_LIBRARY_PATH64_COMPAT}"
+	elif [ -e /libx32/libc.so.6 ] || [ -e /lib32/libc.so.6 ] || [ -e /lib/i386-linux-gnu/libc.so.6 ]; then #32-bit
+		LD_LIBRARY_PATH="${LD_LIBRARY_PATH32}"
+	else
+	  if [ "$(uname -m | grep "64")" != "" ]; then
+	   LD_LIBRARY_PATH="${LD_LIBRARY_PATH64}:${LD_LIBRARY_PATH64_COMPAT}"
+	  else
+	   LD_LIBRARY_PATH="${LD_LIBRARY_PATH32}"
+	  fi
+	fi
+	
+	LD_LIBRARY_PATH="/usr/lib/overrides:${LD_LIBRARY_PATH}"
+	
+	export LD_LIBRARY_PATH
+
+fi
 	
 #[ -L /etc/localtime ] && export TZ="$(readlink /etc/localtime | sed -e 's,/usr/share/zoneinfo/,,' -e 's,Etc/,,')" 
 
@@ -9,7 +34,7 @@ export PATH="/usr/lib/puppy/sbin:/usr/lib/puppy/bin:/usr/sbin/overrides:/usr/bin
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_DIRS=/usr/lib/puppy/share:/usr/local/share:/usr/share
-export XDG_CONFIG_DIRS=/usr/lib/puppy/etc/xdg:/usr/local/etc/xdg:/etc/xdg #v2.14 changed from /usr/etc
+export XDG_CONFIG_DIRS=/usr/lib/puppy/etc/xdg:/usr/local/etc/xdg:/usr/etc/xdg:/etc/xdg #v2.14 changed from /usr/etc
 export XDG_CACHE_HOME=$HOME/.cache
 export XDG_STATE_HOME="$HOME/.local/state"
 #export XDG_RUNTIME_DIR=/run/runtime-${USER}
