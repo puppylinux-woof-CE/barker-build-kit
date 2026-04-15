851c5f721abb18f7735b7649040f0078de6d9bfc
Rizal Martin <49640119+rizalmart@users.noreply.github.com>
2026-04-15 10:30:10 +0800

    Modify os-release file in the root filesystem
    Update os-release file with new OS release information.

--- 01-build-debian-puppy.sh
+++ 01-build-debian-puppy.sh
@@ -517,9 +517,12 @@ chroot ${WKGBASE}/${ROOTFS} /usr/lib/puppy/bin/dpkg-split-status-file
 
 echo "" > ${WKGBASE}/${ROOTFS}/etc/resolv.conf
 
-#rm -f ${WKGBASE}/${ROOTFS}/etc/os-release
-#cp -f ${WKGBASE}/${ROOTFS}/usr/lib/os-release ${WKGBASE}/${ROOTFS}/etc/os-release
-#sed -i -e 's#^NAME=.*#NAME="'$OS_RELEASE_NAME'"#' ${WKGBASE}/${ROOTFS}/etc/os-release
+rm -f ${WKGBASE}/${ROOTFS}/etc/os-release
+cp -f ${WKGBASE}/${ROOTFS}/usr/lib/os-release ${WKGBASE}/${ROOTFS}/etc/os-release
+
+sed -i -e 's#^PRETTY_NAME=.*#PRETTY_NAME="'${OS_RELEASE_PRETTY_NAME}'"#' ${WKGBASE}/${ROOTFS}/etc/os-release
+sed -i -e 's#^NAME=.*#NAME="'${OS_RELEASE_NAME}'"#' ${WKGBASE}/${ROOTFS}/etc/os-release
+sed -i -e 's#^VERSION_CODENAME=.*#VERSION_CODENAME="'${DEBIAN_VERSION_NAME}'"#' ${WKGBASE}/${ROOTFS}/etc/os-release
 
 chroot ${WKGBASE}/${ROOTFS} update-cache.sh y
 
